5603bfe9b89bf8c61a2256cca83aa9058b357e54
Brian <briandsalemi@gmail.com>
2019-07-03 15:50:56 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   
     post '/recipes' do 
       @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
-      redirect to '/recipes'
+      redirect to "/recipes/#{@recipe.id}"
     end 
 
     get '/recipes/:id' do
@@ -26,10 +26,10 @@ class ApplicationController < Sinatra::Base
     end 
 
 
-    delete '/recipes/:id/delete' do 
+    delete '/recipes/:id' do 
       @recipe = Recipe.find(params[:id])
-      @recipe.delete
-      redirect to '/recipes'
+      @recipe.destroy
+      redirect '/recipes'
     end
 
     get '/recipes/:id/edit' do 
